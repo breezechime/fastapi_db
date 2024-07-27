bdf34f2bd9b57aaf81670e80d902638cc1bdecc2
我是指针* <chinadlay@163.com>
2024-07-27 18:25:53 +0800

    - README.md

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README.MD', 'r', encoding='utf-8') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
